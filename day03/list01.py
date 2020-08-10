914be7aa794bcac4a137b3b24c6fe5f5b516fa46
Manikant Jha <mjontop0602@gmail.com>
2020-08-10 17:35:20 +0530

    list by for

--- day03/list01.py
+++ day03/list01.py
@@ -7,12 +7,14 @@
 # student7 = 20
 # print(student1)
 # print(student2)
-#           0   1   2   3   4   5   6   7  8
+#           0   1   2   3   4   5   6   7  8    9 10
 student = [10, 15, 14, 13, 17, 19, 20, 45, 96, 458, 452]
 
-i=0
+# i=0
 
-while i<len(student):
-    print("Student", i+1 , '= ', student[i])
-    i += 1
-
+# while i<len(student):
+#     print("Student", i+1 , '= ', student[i])
+#     i += 1
+#         range(0, 11, 1)
+for i in range(len(student)):
+    print("Student", i+1, "=", student[i]) 
