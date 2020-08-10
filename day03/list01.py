6ec945e506346b048ffb7211f8e2b9281fa65918
Manikant Jha <mjontop0602@gmail.com>
2020-08-10 17:15:47 +0530

    access array by while loops

---
+++ day03/list01.py
@@ -0,0 +1,18 @@
+# student1 = 10
+# student2 = 15
+# student3 = 14
+# student4 = 13
+# student5 = 17
+# student6 = 19
+# student7 = 20
+# print(student1)
+# print(student2)
+#           0   1   2   3   4   5   6   7  8
+student = [10, 15, 14, 13, 17, 19, 20, 45, 96, 458, 452]
+
+i=0
+
+while i<len(student):
+    print("Student", i+1 , '= ', student[i])
+    i += 1
+
