4078f587caaad97eec813e48a1c08fdf4b4a1525
Manikant Jha <mjontop0602@gmail.com>
2020-08-11 16:33:19 +0530

    strings1

---
+++ day04/file0.py
@@ -0,0 +1,12 @@
+a = 10
+b = 15 
+
+print('a =', a)
+print('b =', b)
+
+a = a+b
+b = a-b
+a = a-b
+
+print('a =', a)
+print('b =', b)
---
+++ day04/string01.py
@@ -0,0 +1,29 @@
+a = 'batman'
+b = "superman"
+
+# lista = list(a)
+# print(lista)
+
+# slicing
+
+#  0 1 2 3 4 5
+#  b a t m a n
+               # 0          1           2       3           4           5       6 
+superheroes = ['batman', 'superman', 'flash', 'joker', 'iron man', 'captain', 'arrow']
+
+# print(a[2:])
+
+# print(a[1:4])
+
+# print(superheroes[::2])
+# print(superheroes[6:-1:-1])
+# print(superheroes[-2])
+# print(superheroes[::-1])
+
+# c = a[0].upper()+a[1:]
+
+# print(c)
+
+s1 = 'hello'
+s1 = s1 + ' world'
+print(s1)
